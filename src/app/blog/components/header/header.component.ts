696c93175bff7fa1c0f689a275b62b7b0d15c83e
pabcha <pablochavez15@hotmail.com>
2021-04-07 14:22:28 -0300

    update menu items when authenticated

--- src/app/blog/components/header/header.component.ts
+++ src/app/blog/components/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from './../../../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  isAuthenticated = false;
+  constructor(private _authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this._authService.isAuthenticated$
+      .subscribe(isAuth => this.isAuthenticated = isAuth);
   }
 
 }
